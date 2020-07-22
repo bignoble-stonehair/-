d230cd7c49c62ab999aeb1cc090665aed76d8cd7
bignoble-stonehair <44014369+bignoble-stonehair@users.noreply.github.com>
2020-07-23 00:21:59 +0900

    1.1_v2を追記

---
+++ Chapter1_1_v2.py
@@ -0,0 +1,26 @@
+﻿def isUniqueChars(str):
+    str_flag = 0
+    if len(str) > 128:
+        return False;
+    else:
+        for i in range(len(str)):
+             print(1<<ord(str[i]))
+             if(str_flag & (1<<ord(str[i])) == 0):
+                 str_flag |= 1<<ord(str[i])
+                 
+             else:
+                 return False;
+            
+    return True;
+
+if(isUniqueChars("xirsag")):
+    print("重複しません")
+else:
+    print("重複します")
+
+                 
+
+     
+
+         
+
