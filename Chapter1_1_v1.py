adc50659c24705e2e60771a9b0a2eb5fad27d31a
bignoble-stonehair <44014369+bignoble-stonehair@users.noreply.github.com>
2020-07-22 20:00:30 +0900

    Chaptr1_1_v1

---
+++ Chapter1_1_v1.py
@@ -0,0 +1,18 @@
+﻿str1 = 'apple'
+str2 = 'peach'
+str3 = 'deoxyribonucleic'
+temp = ' '
+flag = True
+len = 0
+for char in str3:
+    for char_temp in temp:
+        if(char_temp == char):
+            flag = False
+            break;
+        else:
+            temp += char   
+    if(not flag):
+        break;
+
+if(flag):
+    print("重複しません")
