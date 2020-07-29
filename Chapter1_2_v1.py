d941c7cab593b270c8c68efe43b9c0f246833ac7
bignoble-stonehair <44014369+bignoble-stonehair@users.noreply.github.com>
2020-07-29 18:17:37 +0900

    Chapter1_2追加

---
+++ Chapter1_2_v1.py
@@ -0,0 +1,14 @@
+﻿def isOrdered(str1, str2):
+    ordered_list1 = sorted(str1)
+    ordered_str1 = ''.join(ordered_list1)
+    ordered_list2 = sorted(str2)
+    ordered_str2 = ''.join(ordered_list2)
+    if ordered_str1 == ordered_str2:
+        return True
+    else:
+        return False
+
+if isOrdered('apple', 'Apple'):
+    print("一致")
+else:
+    print("違うよ")
---
+++ Chapter1_2_v2.py
@@ -0,0 +1,22 @@
+﻿import numpy as np
+
+def isOrdered(str1, str2):
+    ascii_list = np.zeros(128)
+    if(len(str1) != len(str2)):
+        return False
+    else:
+        for char in str1:
+            ascii_list[ord(char)] = ascii_list[ord(char)] + 1
+        for char in str2:
+            ascii_list[ord(char)] = ascii_list[ord(char)] - 1
+            if ascii_list[ord(char)] < 0:
+                return False
+        return True
+   
+if isOrdered('apple', 'apple'):
+    print("一致")
+else:
+    print("違うよ")
+
+
+
