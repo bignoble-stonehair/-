86d09e0d91ae42b3ba84143179147eb7a8720566
bignoble-stonehair <44014369+bignoble-stonehair@users.noreply.github.com>
2020-08-06 17:38:43 +0900

    chapter1_3追加

---
+++ Chapter1_3_v1.py
@@ -0,0 +1,8 @@
+﻿def change_blank_into_char(str):
+    str2 = ""
+    for char in str:
+        if char == ' ':
+            str2 += '%20'
+        else:
+            str2 += char
+    return str2
---
+++ Chapter1_3_v2.java
@@ -0,0 +1,33 @@
+﻿class ChangeBlank{
+static String changeBlankIntoChar(String str, int length){
+    int blank_counter = 0;
+    int i = 0;
+    char[] str_c = str.toCharArray();
+    for (i = 0; i < length; i++){
+        if(str_c[i] == ' '){
+            blank_counter++;
+        }
+    }
+    int new_length = length + 3 * blank_counter;
+    char new_str_c[] = new char[new_length];
+    int index = 0;
+    for(i=0; i<length; i++){
+        if(str_c[i] == ' '){
+            new_str_c[index++] = '%';
+            new_str_c[index++] = '2';
+            new_str_c[index] = '0';
+            index++;
+        }
+        else{
+            new_str_c[index] = str_c[i];
+            index++;
+        }
+    }
+    String result = new String(new_str_c);
+    return result;
+}
+
+public static void main(String[] args){
+    System.out.println(changeBlankIntoChar("a pp le", 7));
+}
+}
