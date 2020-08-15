3bd68d28f829edf7ccb4ca530342957bb3202b9d
bignoble-stonehair <44014369+bignoble-stonehair@users.noreply.github.com>
2020-08-15 23:52:54 +0900

    chapter1_4追加

---
+++ Chapter1_4_v1.py
@@ -0,0 +1,15 @@
+﻿import numpy as np
+
+def is_kaibun(str):
+	a = np.zeros(1000)
+	for c in str:
+		if a[ord(c)] > 0:
+			a[ord(c)] -= 1
+		else:
+			a[ord(c)] += 1
+	if a.sum() > 1:
+		return False
+	else:
+		return True
+
+	
---
+++ Chapter1_4_v2.py
@@ -0,0 +1,18 @@
+﻿def is_kaibun(str):
+    flag = 0
+    for char in str:
+        c = ord(char) - ord('a')
+        c_bit = 2 ** c
+        flag = flag ^ c_bit
+    if flag == 0:
+        return True
+    else:
+        flag = flag & (flag - 1)
+        if flag == 0:
+            return True
+        else: 
+            return False
+
+
+
+        
