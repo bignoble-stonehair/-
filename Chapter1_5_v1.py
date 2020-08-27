648c5a5da1b33d94189b3aed7be7fc2cf3f6aa5f
bignoble-stonehair <44014369+bignoble-stonehair@users.noreply.github.com>
2020-08-27 19:16:09 +0900

    Chapter1_5追加

---
+++ Chapter1_5_v1.py
@@ -0,0 +1,35 @@
+﻿def is_similary(str1, str2):
+    flag = False
+    if abs(len(str1) - len(str2)) == 1:
+        if len(str1) < len(str2):
+            tmp_str = str1
+            str1 = str2
+            str2 = tmp_str
+        i = 0
+        for c in str1:
+            if len(str2) > i:
+                if str2[i] == c:
+                    i += 1
+                elif str2[i] != c and flag == False:
+                    flag = True
+                elif str2[i] != c and flag == True:
+                    return False
+        return True
+    elif len(str1) == len(str2):
+        i = 0
+        for c in str1:
+            if str2[i] == c:
+                i += 1
+            elif str2[i] != c and flag == False:
+                flag = True
+                i += 1
+            elif str2[i] != c and flag == True:
+                return False
+        return True
+    else:
+        return False
+
+if is_similary("pale", "bake"):
+    print("True")
+else:
+    print("False")
---
+++ Chapter1_5_v2.py
@@ -0,0 +1,30 @@
+﻿def is_one_edit_away(first, second):
+    if abs(len(first) - len(second) > 1):
+        return False
+    if len(first) < len(second):
+        s1 = first
+        s2 = second
+    else:
+        s1 = second
+        s2 = first
+    index1 = 0
+    index2 = 0
+    foundDiffrence = False
+    while index1 < len(s1) and index2 < len(s2):
+        if s1[index1] != s2[index2]:
+            if foundDiffrence:
+                return False
+            foundDiffrence = True
+            if len(s1) == len(s2):
+                index1 += 1
+            index2 += 1
+        else:
+            index1 += 1
+            index2 += 1
+    return True
+
+if is_one_edit_away("pale", "bake"):
+    print("True")
+else:
+    print("False")
+
